4d85e62b264d9421f9259642ff21acf108816f3e
Nqobile <nqobilemasombuka@iclud.com>
2025-04-14 11:00:36 +0200

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
-import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -21,7 +20,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -40,8 +38,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-connectDB();
-
 // Test Redis functionality
 redis.set("server", "active");
 redis.get("server", (err, result) => {
@@ -54,4 +50,5 @@ redis.get("server", (err, result) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
